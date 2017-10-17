d129746ca702dd644f002afc2631641662a00742
Ferran Romero <ferranromerogallardo@enti.cat>
2017-10-17 13:34:57 +0200

    Cosas de clase done (No hay audio cagondios)

--- GameProject/src/A05/A05Base.cpp
+++ GameProject/src/A05/A05Base.cpp
@@ -1,13 +1,29 @@
 #include <SDL.h>		// Always needs to be included for an SDL app
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
 
 //Game general information
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
+/*typedef enum 
+{
+		IMG_INIT_JPG = 0x00000001,
+		IMG_INIT_PNG = 0x00000002,
+		IMG_INIT_TIF = 0x00000004,
+		IMG_INIT_WEBP = 0x00000008,
+} IMG_InitFlags;*/
 
 int main(int, char*[]) {
 
 	// --- INIT ---
+	const Uint8 imgFlags = IMG_INIT_PNG | IMG_INIT_JPG;
+	const Uint8 mixFlags{ MIX_INIT_MP3 | MIX_INIT_OGG };
+	if (!(Mix_Init(mixFlags) &mixFlags)) throw "Error_ SDL_mixer init";
+	if (!(IMG_Init(imgFlags) & imgFlags)) throw "Error: SDL_image init";
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) throw "No es pot inicialitzar SDL subsystems";
+	if (TTF_Init() != 0) throw "No es pot inicialitzar SDL_ttf";
 
 	// --- WINDOW ---
 	SDL_Window *window{ SDL_CreateWindow("SDL...", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN) };
@@ -18,12 +34,38 @@ int main(int, char*[]) {
 	if (renderer == nullptr) throw "No es pot inicialitzar SDL_Renderer";
 
 	// --- SPRITES ---
+	SDL_Texture *bgTexture{ IMG_LoadTexture(renderer, "../../res/img/bg.jpg") };
+	if (bgTexture == nullptr) throw "No s'han pogut crear les textures";
+	SDL_Texture *playerTexture{ IMG_LoadTexture(renderer, "../../res/img/kintoun.png") };
+	if (playerTexture == nullptr) throw "No s'han pogut crear les textures";
+	SDL_Rect playerRect{ 0,0,250,250 };
+	SDL_Rect playerTarget{ 0,0,100,100 };
+
+	SDL_Rect bgRect{ 0,0,SCREEN_WIDTH, SCREEN_HEIGHT };
+
+
 
 		// --- Animated Sprite ---
 
 	// --- TEXT ---
+	TTF_Font *font{ TTF_OpenFont("../../res/ttf/saiyan.ttf", 80) };
+	if (font == nullptr) throw "No es pot inicialitzar the TTF_Font";
+	SDL_Surface *tmpSurf{ TTF_RenderText_Blended(font, "Ferran the game", SDL_Color{255,150,0,255}) };
+	if (tmpSurf == nullptr) TTF_CloseFont(font), throw "Unable to create the SDL text surface";
+	SDL_Texture *textTexture{ SDL_CreateTextureFromSurface(renderer, tmpSurf) };
+	SDL_Rect textRect{100, 50, tmpSurf->w, tmpSurf->h};
+	SDL_FreeSurface(tmpSurf);
+	TTF_CloseFont(font);
 
 	// --- AUDIO ---
+	if (Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 1024) == -1) {
+		throw "Unable to initilize SDL_mixer audio system";
+	}
+
+	Mix_Music *soundtrack{ Mix_LoadMUS("../../res/au/mainTheme.mp3") };
+	if (!soundtrack) throw "Unable to load the Mix_Music soundtrack";
+	Mix_VolumeMusic(MIX_MAX_VOLUME);
+	Mix_PlayMusic(soundtrack, -1);
 
 	// --- GAME LOOP ---
 	SDL_Event event;
@@ -34,25 +76,44 @@ int main(int, char*[]) {
 			switch (event.type) {
 			case SDL_QUIT:		isRunning = false; break;
 			case SDL_KEYDOWN:	if (event.key.keysym.sym == SDLK_ESCAPE) isRunning = false; break;
+			case SDL_MOUSEMOTION:
+				playerTarget.x = event.motion.x - 125;
+				playerTarget.y = event.motion.y - 125;
 			default:;
 			}
 		}
+		
+		//SDL_RenderPresent(renderer);
+
 
 		// UPDATE
+		playerRect.x += (playerTarget.x - playerRect.x)/10;
+		playerRect.y += (playerTarget.y - playerRect.y)/10;
 
 		// DRAW
 			//Background
 		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, bgTexture, nullptr, &bgRect);
+		SDL_RenderCopy(renderer, textTexture, nullptr, &textRect);
+		SDL_RenderCopy(renderer, playerTexture, nullptr, &playerRect);
+		SDL_RenderPresent(renderer);
 			//Animated Sprite
 		SDL_RenderPresent(renderer);
 
 	}
 
 	// --- DESTROY ---
+	SDL_DestroyTexture(bgTexture);
+	SDL_DestroyTexture(playerTexture);
+	SDL_DestroyTexture(textTexture);
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 
 	// --- QUIT ---
+	Mix_CloseAudio();
+	IMG_Quit();
+	TTF_Quit();
+	Mix_Quit();
 	SDL_Quit();
 	return 0;
 }
